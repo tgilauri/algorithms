b6f2949382d3590b42bea26146d56e01f177a491
Timur <timur.gilauri@hotmail.com>
2023-04-28 23:01:02 +0300

    merge linked list

--- lib/linked_list.py
+++ lib/linked_list.py
@@ -1,15 +1,16 @@
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val=0, next=None, name=None):
         self.val = val
         self.next = next
+        self.name = name
 
 
-def get_sorted_linked_list(amount):
-    node = ListNode(val=1, next=None)
+def get_sorted_linked_list(amount, start_val=1, prefix=''):
+    node = ListNode(val=start_val, next=None, name='_'.join([prefix, str(start_val)]))
     result = [node]
 
-    for i in range(2, amount + 1, 1):
-        next_node = ListNode(val=i, next=None)
+    for i in range(start_val + 1, start_val + amount + 1, 1):
+        next_node = ListNode(val=i, next=None, name='_'.join([prefix, str(i)]))
         node.next = next_node
         node = next_node
         result.append(node)
--- merge-linked-lists/index.py
+++ merge-linked-lists/index.py
@@ -6,6 +6,30 @@ def sort(a, b):
     return a.val - b.val
 
 
+def merge_linked_lists(head_a, head_b):
+    cur_a, cur_b = [head_a, head_b] if head_a.val <= head_b.val else [head_b, head_a]
+    head = cur_a
+
+    while cur_a.next or cur_b.next:
+        if cur_a.next is None:
+            cur_a.next = cur_b
+            break
+        if cur_b.next is None:
+            cur_b.next = cur_a
+            break
+        if cur_a.val <= cur_b.val <= cur_a.next.val:
+            tmp_cur_a_next = cur_a.next
+            tmp_cur_b_next = cur_b.next
+
+            cur_a.next = cur_b
+            cur_a = cur_b
+            cur_a.next = tmp_cur_a_next
+            cur_b = tmp_cur_b_next
+        else:
+            cur_a = cur_a.next
+    return head
+
+
 def merge_k_lists(lists):
     items = []
     for head in lists:
@@ -22,11 +46,12 @@ def merge_k_lists(lists):
 
 
 linked_list = get_sorted_linked_list(5)
-linked_list_second = get_sorted_linked_list(5)
+linked_list_second = get_sorted_linked_list(4, 3)
 linked_list_third = get_sorted_linked_list(5)
 
-print(merge_k_lists([
-    linked_list[0],
-    linked_list_second[0],
-    linked_list_third[0],
-]))
+# head_sorted = merge_linked_lists(linked_list[0], linked_list_second[0])
+
+head_sorted = merge_linked_lists(get_sorted_linked_list(amount=5, prefix='list_1')[0],
+                                 get_sorted_linked_list(amount=5, prefix='list_2')[0])
+
+print(head_sorted)
