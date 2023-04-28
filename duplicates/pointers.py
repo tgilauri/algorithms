b873af3a9b335f4ec54e99482e8355acc28682d7
Timur <timur.gilauri@hotmail.com>
2023-04-28 14:00:59 +0300

    remove duplicates with two pointers

--- duplicates/pointers.py
+++ duplicates/pointers.py
@@ -1,19 +1,22 @@
 def remove_duplicates(nums):
-    slow = 0
-    fast = 1
-    while fast <= len(nums):
-        if fast >= len(nums):
-            result.append(nums[slow])
-            break
-        if nums[slow] == nums[fast]:
-            nums[fast] = None
-            fast += 1
-            continue
-
-        result.append(nums[slow])
-        slow = fast
-        fast += 1
-    return result
+    result = []
+    for n in nums:
+        if not result or n > result[-1]:
+            result.append(n)
+    return result + ['_'] * (len(nums) - len(result))
 
 
-print(remove_duplicates([0, 0, 1, 1, 1, 2, 3, 3, 3, 4, 5, 5, 5, 5, 5, 5]))
+def removeDuplicates(nums):
+    pntr_slow = 1  # позиция, на котрую мы ставим новое значение
+    for pntr_fast in range(1, len(nums)):
+        if nums[pntr_fast] > nums[pntr_fast - 1]:
+            nums[pntr_slow] = nums[pntr_fast]
+            pntr_slow += 1
+    for pntr_slow in range(pntr_slow, len(nums)):
+        nums[pntr_slow] = "_"
+    return nums
+
+
+print(removeDuplicates([0, 1, 1, 1, 2, 3, 3, 3, 4, 5, 5, 5, 5, 5, 5, 6]))
+print(remove_duplicates([]))
+print(remove_duplicates([1]))
