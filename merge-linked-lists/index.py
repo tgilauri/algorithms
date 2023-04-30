768ceadc50e04e7749f958a458382b280c8df917
Timur <timur.gilauri@hotmail.com>
2023-04-30 12:28:08 +0300

    merge linked list

--- merge-linked-lists/index.py
+++ merge-linked-lists/index.py
@@ -7,17 +7,21 @@ def sort(a, b):
 
 
 def merge_linked_lists(head_a, head_b):
+    if head_a is None:
+        return head_b
+    elif head_b is None:
+        return head_a
+
     cur_a, cur_b = [head_a, head_b] if head_a.val <= head_b.val else [head_b, head_a]
     head = cur_a
 
-    while cur_a.next or cur_b.next:
+    while True:
         if cur_a.next is None:
             cur_a.next = cur_b
             break
-        if cur_b.next is None:
-            cur_b.next = cur_a
+        if cur_b is None:
             break
-        if cur_a.val <= cur_b.val <= cur_a.next.val:
+        if cur_b.val <= cur_a.next.val:
             tmp_cur_a_next = cur_a.next
             tmp_cur_b_next = cur_b.next
 
@@ -31,18 +35,11 @@ def merge_linked_lists(head_a, head_b):
 
 
 def merge_k_lists(lists):
-    items = []
-    for head in lists:
-        curr = head
-        while curr:
-            items.append(curr)
-            curr = curr.next
-    sorted_items = sorted(items, key=cmp_to_key(sort))
-    for i in range(len(sorted_items) - 1, -1, -1):
-        item = sorted_items[i]
-        if i != 0:
-            sorted_items[i - 1].next = item
-    return sorted_items[0]
+    list_a = lists[0]
+    for i in range(1, len(lists)):
+        list_b = lists[i]
+        list_a = merge_linked_lists(list_a, list_b)
+    return list_a
 
 
 linked_list = get_sorted_linked_list(5)
@@ -51,7 +48,7 @@ linked_list_third = get_sorted_linked_list(5)
 
 # head_sorted = merge_linked_lists(linked_list[0], linked_list_second[0])
 
-head_sorted = merge_linked_lists(get_sorted_linked_list(amount=5, prefix='list_1')[0],
-                                 get_sorted_linked_list(amount=5, prefix='list_2')[0])
+head_sorted = merge_k_lists([get_sorted_linked_list(amount=5, prefix='list_1')[0],
+                             get_sorted_linked_list(amount=5, prefix='list_2')[0]])
 
 print(head_sorted)
