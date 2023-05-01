f4dbea2207b790c7200ae37f8f8be55b23c4797b
Timur <timur.gilauri@hotmail.com>
2023-05-01 15:46:12 +0300

    two sum with pointers

--- two-sum/index.py
+++ two-sum/index.py
@@ -9,4 +9,20 @@ def two_sum(nums, target: int):
     return result
 
 
-print(two_sum(nums=[2, 7, 11, 15], target=9))
+def two_sum_pntr(nums, target):
+    result = []
+    slow = 0
+    fast = 1
+    while slow < len(nums) - 1:
+        if nums[slow] + nums[fast] == target:
+            result.append(slow)
+            result.append(fast)
+            break
+        fast += 1
+        if fast == len(nums) - 1:
+            slow += 1
+            fast = slow + 1
+    return result
+
+
+print(two_sum_pntr(nums=[12, 36, 14, 2, 22, 11, 14, 13, 12, 19, 3, 4, 2, 7, 11, 15], target=9))
