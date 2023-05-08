5f0d72c7a4ee2b9cf8aecf666c8acca433298309
Timur <timur.gilauri@hotmail.com>
2023-05-08 23:39:48 +0300

    two sum for O(n)

--- two-sum/index.py
+++ two-sum/index.py
@@ -31,5 +31,17 @@ def two_sum_sorted_pntr(nums, target):
     return result
 
 
+def two_sum_with_dict(nums, target):
+    hash_map = {}
+
+    for num in nums:
+        if (target - num) in hash_map:
+            return [target - num, num]
+        if num not in hash_map:
+            hash_map[num] = True
+    return None
+
+
 print(two_sum_pntr(nums=[12, 36, 14, 2, 22, 11, 14, 13, 12, 19, 3, 4, 2, 7, 11, 15], target=9))
 print(two_sum_sorted_pntr(nums=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], target=9))
+print(two_sum_with_dict(nums=[2, 7, 3, 8, 4, 9, 5, 1], target=12))
